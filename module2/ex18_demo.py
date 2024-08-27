b4c615bf7ac2115eeecfdf2ed2fd8de6bfee296c
logan-tolbert <logo88205+git@gmail.com>
2024-08-27 03:57:18 -0500

    :construction: Add code examples

---
+++ module2/ex18_demo.py
@@ -0,0 +1,16 @@
+def do_nothing():
+    pass
+
+def do_something():
+    print("I did something!")
+    
+# now we can call it by iits name
+do_something()
+
+def do_more_things(a,b):
+    # a = "hello"
+    # b = 1
+    print("A IS", a, "B IS", b)
+    
+do_more_things("hello", 1)
+    
